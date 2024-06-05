ff2b75372bd99142126934c3edb8ae0a49948e9e
Ben Reinert <146287616+Soccertoad@users.noreply.github.com>
2024-06-05 13:59:30 -0400

    Adding hard coded speed Commands

---
+++ Understanding-Robot-Control/src/main/java/frc/robot/Commands/Forward.java
@@ -0,0 +1,46 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.Commands;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.Subsystems.Drivebase;
+
+public class Forward extends Command {
+  /*    Creating Variables */
+  //      Creates Drivebase subsystem
+  private Drivebase drivebase;
+  //      Creates Boolean that ends the command
+  private Boolean endCommand = false;
+
+  /**   Creates a new Forward. */
+  public Forward(Drivebase m_drivebase) {
+    //      Makes local variable equal to the global variable
+    drivebase = m_drivebase; 
+
+    //      Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(drivebase);
+  }
+
+  //    Called when the command is initially scheduled.
+  @Override
+  public void initialize() {}
+
+  //    Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    //      Calls the forward method from the Drivebase subsystem
+    drivebase.forward();
+  }
+
+  //    Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {}
+
+  //    Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return endCommand;
+  }
+}
